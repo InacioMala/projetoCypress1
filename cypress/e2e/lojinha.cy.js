1c0cc48f65e2c570846a936542a15fc8666fe8ee
danilosic <danilosic@hotmail.com>
2026-03-25 21:14:12 -0300

    Primeira subida do projeto com cypress

---
+++ cypress/e2e/lojinha.cy.js
@@ -0,0 +1,40 @@
+describe('Suíte de testes do Sistema Lojinha', () => {
+    it('Fazer login e adicionar produto', () => {
+        cy.visit('http://165.227.93.41/lojinha-web/v2/');
+
+        cy.get('#usuario')
+          .click({force: true})
+          .type('admin');
+
+        cy.get('#senha')
+          .click({force: true})
+          .type('admin');
+
+        cy.get('button[type="submit"]').click();
+
+        cy.contains('Boas vindas').should('be.visible');
+
+        cy.contains('a','Adicionar produto').click();
+
+        cy.get('#produtonome')
+          .click({force: true})
+          .type('Playstation 5');
+
+        cy.get('#produtovalor')
+          .click({force: true})
+          .type('4500');
+        
+        cy.get('#produtocores')
+          .click({force: true})
+          .type('Branco');
+
+        cy.get('#btn-salvar').click();
+
+        cy.contains('a','Lista de Produtos').click();
+
+        cy.contains('Lista de Produtos').should('be.visible');
+
+        cy.screenshot();
+        
+    });
+});
